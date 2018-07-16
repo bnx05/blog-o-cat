edf68d7ca37c8321f9e9e220f6b9bf7442ff0ee3
bnx05 <bnx05@users.noreply.github.com>
2018-07-17 01:29:06 +0800

    Bootstrap changes (#8)
    * add field
    
    * remove css
    
    * delete templates
    
    * add urls and views
    
    * use new index template
    
    * add new static files
    
    * add requirements

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.home, name='home'),
+    url(r'^about', views.about, name='about'),
+    url(r'^blog', views.post_list, name='blog'),
+    url(r'^contact', views.contact_page, name='contact'),
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,18 @@ from .forms import PostForm
 from .models import Post
 
 
+def about(request):
+    return render(request, 'blog/about.html')
+
+
+def contact_page(request):
+    return render(request, 'blog/contact.html')
+
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -34,7 +46,7 @@ def post_edit(request, pk):
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/blog.html', {'posts': posts})
 
 
 @login_required
