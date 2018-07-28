26f236717e8387d722eeaab242d3836308facd5b
bnx05 <banquil_05ive@yahoo.com>
2018-07-28 23:44:23 +0800

    add slug

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.utils import timezone
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    summary = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
     text = models.TextField()
     created_date = models.DateTimeField(
         default=timezone.now)
@@ -15,6 +17,10 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
