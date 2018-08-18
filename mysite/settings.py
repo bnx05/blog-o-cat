d5d4a9927872dee87718ca650be3e96edd363a1b
bnx05 <banquil_05ive@yahoo.com>
2018-08-18 23:08:31 +0800

    add tinymce config

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+TINYMCE_DEFAULT_CONFIG = {
+    'theme': "advanced",
+    'plugins': "wordcount,preview,emotions,spellchecker,",
+    'height': "500px",
+    'width': "700px",
+    'theme_advanced_buttons3': "backcolor,blockquote,fontselect,fontsizeselect,emotions,preview,spellchecker,",
+}
