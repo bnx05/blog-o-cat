4a90cdda3788d1dcf4bb0ce24f30d094e6470819
bnx05 <bnx05@users.noreply.github.com>
2018-08-18 15:24:53 +0000

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y9bbpr3lrh$rd)j^$b%8i#w%zg7i+8gn44plo6c!0_=5p#-7+4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'irinqa.com']
 
